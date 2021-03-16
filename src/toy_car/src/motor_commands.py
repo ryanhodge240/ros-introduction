899c0a5567df18889a61c0351f61c03799cce146
Ryan Hodge <ryanhodge240@gmail.com>
2021-03-16 16:56:48 -0400

    c

--- src/toy_car/src/motor_commands.py
+++ src/toy_car/src/motor_commands.py
@@ -22,7 +22,7 @@ def setup_motor_controller():
         GPIO.setup(motors[i][2], GPIO.OUT)
         GPIO.setup(motors[i][0], GPIO.LOW)
         GPIO.setup(motors[i][1], GPIO.LOW)
-        motor_pwm[i] = GPIO.PWM(motors[i][2], 1000)
+        motor_pwm.append(GPIO.PWM(motors[i][2], 1000))
         motor_pwm[i].start(25)
 
 def set_pwm_equal():
